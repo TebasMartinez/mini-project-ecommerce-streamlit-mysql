5f02ff9084af62e060a1c811a9d2dd6ad3b90f14
Tebas Martínez <203688711+TebasMartinez@users.noreply.github.com>
2025-08-26 17:45:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,27 @@ import streamlit as st
 def main():
     st.title("Mini Project: E-commerce Store with Streamlit + MySQL")
 
+    # connect to database
+    db = st.secrets["mysql"]
+    engine = create_engine(f"mysql+pymysql://{db.user}:{db.password}@{db.host}/{db.database}")
+
     loggedin = False
     while loggedin == False:
-        # show signup
-            
-        # show login
-        
+        left, right = st.columns(2)
+        if left.button("Sign Up"):
+            st.header("Sign Up")
+            first_name = st.text_input("First Name")
+            last_name = st.text_input("Last name")
+            email = st.text_input("Email")
+            password = st.text_input("Password")
+            if st.button("Submit"):
+                signup(engine, first_name, last_name, email, password)
+
+        if right.button("Log in"):
+            st.header("Log in")
+            email = st.text_input("Enter your email")
+            password = st.text_input("Enter your password")
+
         # once logged in
         loggedin = True
 
@@ -23,11 +38,20 @@ def main():
 
     # button to show previous orders
 
-def signup():
+def signup(engine, first_name, last_name, email, password):
+    with engine.connect() as connection:
+        txt = f'''INSERT INTO customers (first_name, last_name, email, password)
+                  VALUES ("{first_name}", "{last_name}", "{email}", "{password}");'''
+        query = text(txt)
+        connection.execute(query)
+        connection.commit()
+        return True
+
+def login():
     #to do
     pass
 
-def login():
+def displayproducts():
     #to do
     pass
 
