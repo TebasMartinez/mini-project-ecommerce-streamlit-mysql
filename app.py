6c5d8296195ef8c7d1e20b9e1a2c9607c569c626
Tebas Martínez <203688711+TebasMartinez@users.noreply.github.com>
2025-08-26 18:00:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,18 +10,23 @@ def main():
     db = st.secrets["mysql"]
     engine = create_engine(f"mysql+pymysql://{db.user}:{db.password}@{db.host}/{db.database}")
 
+
+    # FIRST PAGE
     loggedin = False
     while loggedin == False:
-        left, right = st.columns(2)
-        if left.button("Sign Up"):
-            st.header("Sign Up")
-            first_name = st.text_input("First Name")
-            last_name = st.text_input("Last name")
-            email = st.text_input("Email")
-            password = st.text_input("Password")
-            if st.button("Submit"):
-                signup(engine, first_name, last_name, email, password)
+        left, right = st.columns(2) # creates 2 columns
 
+        # Left button, sign up
+        # if left.button("Sign Up"):
+        st.header("Sign Up")
+        first_name = st.text_input("First Name")
+        last_name = st.text_input("Last name")
+        email = st.text_input("Email")
+        password = st.text_input("Password")
+        if st.button("Submit"):
+            signup(engine, first_name, last_name, email, password)
+
+        # Right button, log in
         if right.button("Log in"):
             st.header("Log in")
             email = st.text_input("Enter your email")
@@ -55,17 +60,21 @@ def displayproducts():
     #to do
     pass
 
-def addtocart():
-    #to do
-    pass
+def showorders():
+     # to do
+     pass
 
 def buy():
     #to do
     pass
 
-def showorders():
-     # to do
-     pass
+def updatecart():
+    #to do
+    pass
+
+def updatewishlist():
+    # to do
+    pass
 
 if __name__ == '__main__':
     main()
