6f37aaeb2f9a662978895d2a77a6be7c281a5dd3
Tebas Martínez <203688711+TebasMartinez@users.noreply.github.com>
2025-08-26 15:58:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
+import getpass 
+import pandas as pd
+import pymysql
+from sqlalchemy import create_engine, text
 import streamlit as st
 
 def main():
     st.title("Mini Project: E-commerce Store with Streamlit + MySQL")
+
+def signup():
+    #to do
+    pass
+
+def login():
+    #to do
+    pass
+
+def addtocart():
+    #to do
+    pass
+
+def buy():
+    #to do
+    pass
 
 if __name__ == '__main__':
     main()
